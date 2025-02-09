069d0d77e22ac41c74c84e0fecdbb256b9fd144b
alabasterUbuntu <johnny.liang@ontariotechu.net>
2025-02-09 03:22:30 -0500

    sd

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
       </header>
 
       <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        {isUploadView ? (
+        {!isUploadView ? (
           <div className="bg-white shadow rounded-lg p-6">
             <DocumentUpload />
           </div>
